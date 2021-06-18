fedb6175c892b10edb0a53ffb691b2a25b6a6fe5
Julian Sparber <julian@sparber.net>
2021-06-18 16:06:44 +0200

    common: Add method to get the event id from a SyncRoomEvent

--- matrix_sdk_common/src/deserialized_responses.rs
+++ matrix_sdk_common/src/deserialized_responses.rs
@@ -95,6 +95,13 @@ pub struct SyncRoomEvent {
     pub encryption_info: Option<EncryptionInfo>,
 }
 
+impl SyncRoomEvent {
+    /// Get the event id of this `SyncRoomEvent` if the event has any valid id.
+    pub fn event_id(&self) -> Option<EventId> {
+        self.event.get_field::<EventId>("event_id").ok().flatten()
+    }
+}
+
 impl From<Raw<AnySyncRoomEvent>> for SyncRoomEvent {
     fn from(inner: Raw<AnySyncRoomEvent>) -> Self {
         Self { encryption_info: None, event: inner }
